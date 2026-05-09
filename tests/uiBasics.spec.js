2cb42c751d7dfae017860d9defaa3ad5fd7b38b9
anitrai <anitrai2013@gmail.com>
2026-05-10 00:32:47 +0530

    Revert "changed playwright.yml with workflow_dispatch which allows manual call option"
    This reverts commit b009109016d87720d5159533f6afea43e90571a3.

---
+++ tests/uiBasics.spec.js
@@ -0,0 +1,14 @@
+// const {test} = require("@playwright/test");
+
+// test('first test cases',async function({browser}){
+
+ 
+
+// })
+
+// multiples of 2 & 5
+
+for(let i = 1;i<=10;i++){
+    if(i%2 == 0 && i%5 == 0)
+        console.log("Multiples of 2 & 5:", i);
+}
